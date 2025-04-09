b15476eb1fc527aa59c8341af7de5f3b33a95950
johnbabyjoel <joelbabyjohn026@gmail.com>
2025-04-09 14:55:24 +0530

    model is added

--- jd_sports_app/models.py
+++ jd_sports_app/models.py
@@ -1,1 +1,13 @@
 
+from django.db import models
+
+class Jersey(models.Model):
+    name = models.CharField(max_length=100)
+    team = models.CharField(max_length=100)
+    size = models.CharField(max_length=10)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to='jersey_images/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.team})"
