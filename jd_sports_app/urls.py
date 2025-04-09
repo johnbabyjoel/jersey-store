f4e2a0cfd22bfc6b96acf06d00042bace8fec4ad
johnbabyjoel <joelbabyjohn026@gmail.com>
2025-04-09 14:53:20 +0530

    Edit functionality added

--- jd_sports_app/urls.py
+++ jd_sports_app/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='jersey_list'),
     path('add/', views.add_jersey, name='add_jersey'),
+    path('edit/<int:id>/', views.edit_jersey, name='edit_jersey'),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
