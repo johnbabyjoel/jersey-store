63c576257531844813ccca4dc5db3a9f85d95cdd
johnbabyjoel <joelbabyjohn026@gmail.com>
2025-04-10 17:31:55 +0530

    view updated

--- jd_sports_app/views.py
+++ jd_sports_app/views.py
@@ -46,3 +46,10 @@ def edit_jersey(request, id):
 
     return render(request, 'edit_jersey.html', {'jersey': jersey})
     
+def delete_jersey(request, jersey_id):
+    try:
+        jersey = Jersey.objects.get(id=jersey_id)
+        jersey.delete()
+    except Jersey.DoesNotExist:
+        pass
+    return redirect('/')
