e136cbce860cbab0457ddcfceec722e3e24ecf18
johnbabyjoel <joelbabyjohn026@gmail.com>
2025-04-10 17:26:34 +0530

    view updated

--- jd_sports_app/views.py
+++ jd_sports_app/views.py
@@ -3,7 +3,9 @@ from .models import Jersey
 from django.shortcuts import redirect, get_object_or_404
 from django.contrib import messages
 
-
+def index(request):
+    jerseys = Jersey.objects.all()
+    return render(request, 'index.html', {'jerseys': jerseys})
 
 def add_jersey(request):
     if request.method == 'POST':
